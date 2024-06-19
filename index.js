a3db4bc6f346f71c1879e13c30c192278c3d41f2
lakshay394 <lakshay.jangra.394@gmail.com>
2024-06-19 13:46:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 import pg from "pg";
 
 const app = express();
-const port = 10000;
+const port = process.env.PORT || 4000;
 
 const db = new pg.Client({
     user: process.env.PGUSER,
