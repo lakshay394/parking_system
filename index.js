68f1a88db357ac383249e4e92a9a78b96c637b69
lakshay394 <lakshay.jangra.394@gmail.com>
2024-06-19 23:39:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -356,16 +356,21 @@ app.get("/auth/google", passport.authenticate("google", {
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     }, async (accessToken, refreshToken, profile, cb) => {
       try {
-        const result = await db.query("select * from users where email=$1",[profile.email]);
-        if (result.rows.length===0) {
-          const newUser=await db.query("insert into users(email,password) values($1,$2)",[profile.email,"google"]);
-          cb(null,newUser.rows[0]);
+        console.log(profile);
+        const result = await db.query("SELECT * FROM users WHERE email = $1", [
+          profile.email,
+        ]);
+        if (result.rows.length === 0) {
+          const newUser = await db.query(
+            "INSERT INTO users (email, password) VALUES ($1, $2)",
+            [profile.email, "google"]
+          );
+          return cb(null, newUser.rows[0]);
+        } else {
+          return cb(null, result.rows[0]);
         }
-        else{
-          cb(null,result.rows[0]);
-        }
-      } catch (error) {
-        cb(err);
+      } catch (err) {
+        return cb(err);
       }
     })
   );
