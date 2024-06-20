fdf450a7b73f841a6bc5fd2fb73afeeeb98fc747
lakshay394 <lakshay.jangra.394@gmail.com>
2024-06-20 11:10:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -319,7 +319,7 @@ app.post("/register", async (req, res) => {
     ]);
 
     if (checkResult.rows.length > 0) {
-      req.redirect("/login");
+      res.redirect("/login");
     } else {
       bcrypt.hash(password, saltRounds, async (err, hash) => {
         if (err) {
