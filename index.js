cda067f8203c892153f089c1806f26c26a4693d1
lakshay394 <lakshay.jangra.394@gmail.com>
2024-06-20 08:43:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -368,7 +368,7 @@ passport.use("google", new GoogleStrategy(
   {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
+    callbackURL: "https://parking-system-yoyu.onrender.com/auth/google/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
   }, async (accessToken, refreshToken, profile, cb) => {
     try {
